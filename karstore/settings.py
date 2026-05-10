5133a22a409252ba3fa633399520c973c42ed0f0
Aayushkassey <surump3@gmail.com>
2026-05-10 16:35:51 +0545

    added whitenoise storage settings

--- karstore/settings.py
+++ karstore/settings.py
@@ -77,6 +77,7 @@ TEMPLATES =[
 MIDDLEWARE = [ 
     # 'django.middleware.cache.UpdateCacheMiddleware',
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -158,6 +159,9 @@ STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
