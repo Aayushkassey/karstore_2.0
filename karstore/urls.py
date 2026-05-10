13fcb7982e335daed16c95daf283f0663bf099da
Aayushkassey <surump3@gmail.com>
2026-05-10 17:44:28 +0545

    Email system and payemnt with emojissss huuuu

--- karstore/urls.py
+++ karstore/urls.py
@@ -58,8 +58,7 @@ urlpatterns = [
 ]
 
 # Static ra Media handling
-if settings.DEBUG:
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #     urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
 
