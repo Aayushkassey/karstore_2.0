f58524b5e33f98245990d4c22af53a6e9b7e38df
Aayushkassey <surump3@gmail.com>
2026-05-10 18:28:57 +0545

    fix: cloudinary storage added

--- karstore/settings.py
+++ karstore/settings.py
@@ -56,6 +56,16 @@ INSTALLED_APPS += ['django_cleanup.apps.CleanupConfig']
 
 AUTH_USER_MODEL = 'accounts.CustomerUser'
 
+INSTALLED_APPS += ['cloudinary', 'cloudinary_storage']
+
+CLOUDINARY_STORAGE = {
+    'CLOUD_NAME': os.environ.get('CLOUDINARY_CLOUD_NAME'),
+    'API_KEY': os.environ.get('CLOUDINARY_API_KEY'),
+    'API_SECRET': os.environ.get('CLOUDINARY_API_SECRET'),
+}
+
+DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+
 
 TEMPLATES =[
     {
