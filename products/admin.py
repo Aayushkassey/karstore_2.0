117e406db83e8ddf51bfb4735f4be504d1c81f26
Aayushkassey <surump3@gmail.com>
2026-05-11 10:06:03 +0545

    Image loading

--- products/admin.py
+++ products/admin.py
@@ -4,15 +4,17 @@ from django.contrib import admin
 from django.db.models import Sum
 from .models import Product, Category
 
+from django.utils.html import format_html
+
 @admin.register(Product)
 class ProductAdmin(admin.ModelAdmin):
     list_select_related = ('category', 'seller')
-    readonly_fields = ('total_sales_display',)
+    readonly_fields = ('total_sales_display', 'image_preview')
 
     fields = (
         'name', 'seller', 'category',
         'total_sales_display', 'sku', 'brand', 'discount_percentage',
-        'description', 'price', 'stock', 'rating', 'image'
+        'description', 'price', 'stock', 'rating', 'image', 'image_url', 'image_preview'
     )
 
     list_display = (
@@ -32,10 +34,15 @@ class ProductAdmin(admin.ModelAdmin):
     def total_sales_display(self, obj):
         total = getattr(obj, 'total_sales_annotated', None) or 0
         return f"{total} Units"
-
     total_sales_display.short_description = 'Total Sales'
     total_sales_display.admin_order_field = 'total_sales_annotated'
 
+    def image_preview(self, obj):
+        url = obj.display_image
+        if url:
+            return format_html('<img src="{}" width="150" style="object-fit:contain; border-radius:8px;">', url)
+        return "No Image"
+    image_preview.short_description = 'Image Preview'
 
 @admin.register(Category)
 class CategoryAdmin(admin.ModelAdmin):
