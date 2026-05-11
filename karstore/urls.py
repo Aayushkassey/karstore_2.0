3cfd9a9dfadd88c31058247441c481c900244f2a
Aayushkassey <surump3@gmail.com>
2026-05-11 11:35:58 +0545

    Image loadingggssss

--- karstore/urls.py
+++ karstore/urls.py
@@ -59,7 +59,8 @@ urlpatterns = [
 ]
 
 # Static ra Media handling
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+if settings.DEBUG:
+     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 #     urlpatterns = [path('__debug__/', include(debug_toolbar.urls))] + urlpatterns
 
